7e331b22c1fe43a676e32f3028a0a79e1b8d5766
Matt Trim <MattTrim1@users.noreply.github.com>
2022-04-06 15:38:06 +0100

    feat: allow client to receive custom headers (#4)
    Provides the ability for a consumer to define custom headers that should be transmitted in the request. NB: All Headers passed into the client will be applied to every request made with the Client instance that contains the custom headers.

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -32,3 +32,22 @@ test('client throws exception when given invalid authentication type', function 
     expect(fn() => new Client($mockClient, $validMethod, $key))->not->toThrow(RuntimeException::class);
     expect(fn() => new Client($mockClient, $invalidMethod, $key))->toThrow(RuntimeException::class);
 });
+
+test('client allows custom headers to be transmitted in the request', function () {
+   $customHeaders = ['Authorization' => 'Bearer 1234'];
+
+    $mockClient = mock_client();
+    $method = 'basic';
+    $key = 'testKey';
+
+    $client = new Client($mockClient, $method, $key, $customHeaders);
+
+    $reflectionClient = new ReflectionClass($client);
+    $getHeadersToSend = $reflectionClient->getMethod('getHeadersToSend');
+
+    expect($getHeadersToSend->isPrivate())->toBeTrue();
+    $getHeadersToSend->setAccessible(true);
+
+    $headers = $getHeadersToSend->invoke($client);
+    expect($headers['Authorization'])->toContain('Bearer');
+});
